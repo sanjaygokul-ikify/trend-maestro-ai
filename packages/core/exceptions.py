1f392cc8a3b08125dff4b2502a06cf0554456e8a
Sanjay G <gokulesh2615@gmail.com>
2026-07-27 15:16:22 +0530

    feat(errors): implement typed exception hierarchy

---
+++ packages/core/exceptions.py
@@ -0,0 +1,11 @@
+class MaestroAIException(Exception):
+    pass
+
+class EngineException(MaestroAIException):
+    pass
+
+class AgentException(MaestroAIException):
+    pass
+
+class TaskException(MaestroAIException):
+    pass
